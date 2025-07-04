ce6a576fad4c3d1357833fed379739c741bfddff
ololade <akin0098@algonquinlive.com>
2025-07-04 13:23:07 -0400

    corrected

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,4 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {name = "cst8918-lab-rg" location = "East US"}
+resource "azurerm_resource_group" "example" {
+  name = "cst8918-lab-rg"
+  location = "East US"
+}
