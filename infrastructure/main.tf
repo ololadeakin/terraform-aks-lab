aed38461c923aecc2016ddd84856b72a15c6967d
ololade <akin0098@algonquinlive.com>
2025-07-04 14:41:43 -0400

    bad formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example" {\
   name     = "example-resources"
   location = "Canada Central"
 }
