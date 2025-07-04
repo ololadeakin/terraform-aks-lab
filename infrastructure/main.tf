e4adbc4ff93a01d7a9aa831d8cd605e441e1cb8c
ololade <akin0098@algonquinlive.com>
2025-07-04 14:03:14 -0400

    bad formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,8 +2,4 @@ provider "azurerm" {
   features {}
 }
 
-resource    "azurerm_resource_group"      "example"
-{
-name="example-resources"
-location="Canada Central"
-}
+resource"azurerm_resource_group""example"{nam="example-resources"location="Canada Central"}
