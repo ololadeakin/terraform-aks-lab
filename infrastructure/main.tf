7c012a8e8d5a53ceec0abc26b8b2f2694f1b2efc
ololade <akin0098@algonquinlive.com>
2025-07-04 14:37:47 -0400

    Fix formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example"
-  name = "example-resources"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
   location = "Canada Central"
 }
